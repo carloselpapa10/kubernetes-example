1f027353c3e0afec668706813c8be288140b1e86
carloselpapa10 <c.avendano10@gmail.com>
2018-04-17 19:54:32 +0200

    kubernetes files

--- order-service/src/main/docker/wait-for-mysql.sh
+++ order-service/src/main/docker/wait-for-mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while ! exec 6<>/dev/tcp/${DATABASE_HOST}/${DATABASE_PORT}; do
+while ! exec 6<>/dev/tcp/${DATABASE_HOST}/3306; do
     echo "Trying to connect to MySQL at ${DATABASE_PORT}..."
     sleep 10
 done
